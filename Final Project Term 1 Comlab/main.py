f83c4fddf9e20f1b8e72a50fe931bbfc5d78be04
Neil Justin V. Bermoy <njBermoy@mcm.edu.ph>
2024-11-07 19:44:20 +0000

    Create main.py

---
+++ Final Project Term 1 Comlab/main.py
@@ -0,0 +1,21 @@
+"""
+Final Project: Agri-Memory
+Created By Group 1:
+Neil Justin V. Bermoy
+Raffy A. Panawidan
+Neil M. Olbes
+Robert Paul C. Nery
+Created: Month of Oct.
+Modified: 11-08-24
+"""
+
+#Title and descriptions
+
+#Main Function of the entire program
+def main():
+
+
+
+if __name__ == "__main__":
+	main()
+
