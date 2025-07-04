1b7edacc35a7d3c8251461b4378c3913e8a5fdc6
Neil Justin V. Bermoy <njBermoy@mcm.edu.ph>
2025-07-04 13:44:39 +0000

    new

---
+++ Final Project Term 3/Login/src/login/BCBArmoryNexus.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.launcher;
+import main.ui.LoginScreen;
+import main.ui.RegisterScreen;
+import main.ui.SplashScreen;
+import main.ui.AdminDashboard;
+import main.ui.MainCatalogue;
+import main.ui.UserDashboard;
+/**
+ *
+ * @author Admin
+ */
+
+public class BCBArmoryNexus {
+    public static void main(String[] args) {
+             new MainCatalogue().setVisible(true);
+        
+//            new SplashScreen().setVisible(true); // let SplashScreen handle the rest
+//          new LoginScreen().setVisible(true);
+//            new RegisterScreen().setVisible(true);
+//            new AdminDashboard().setVisible(true);
+//                new UserDashboard().setVisible(true);
+ 
+    
+    }
+}
